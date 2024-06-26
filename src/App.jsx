276fa04c0810e500b30acee8c21a2a2980329c2b
Harshal.Ogale-at-447515355797 <harshal.ogale@flentas.com>
2024-06-26 18:52:02 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
 function App() {
+  <div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+      <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
 
-  return (
-  <></>
-  )
+      <div className="w-full max-w-sm mx-auto"></div>
+    </div>
+  </div>;
 }
 
-export default App
+export default App;
